d43361f18f0b87dfe7bb13efddd6d541b2b2e013
Joe Kamien <14943848+joekamien@users.noreply.github.com>
2026-03-20 13:01:12 -0700

    update fruit controller with query params

--- app/controllers/fruits_controller.rb
+++ app/controllers/fruits_controller.rb
@@ -2,6 +2,19 @@ class FruitsController < ApplicationController
   def index
     fruits = Fruit.all
 
+    if params[:name].present?
+      fruits = fruits.where("name ILIKE ?", "%#{params[:name]}%")
+    end
+
+    if params[:in_season].present?
+      in_season = ActiveModel::Type::Boolean.new.cast(params[:in_season])
+      fruits = fruits.where(in_season: in_season)
+    end
+
+    if params[:color].present?
+      fruits = fruits.where("colors @> ARRAY[?]::varchar[]", [ params[:color] ])
+    end
+
     render json: fruits
   end
 end
