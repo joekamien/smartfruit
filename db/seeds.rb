baa1af1bc3f7b45217e24982b38194af6694ce86
Joe Kamien <14943848+joekamien@users.noreply.github.com>
2026-03-20 12:28:12 -0700

    seeds file to add data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'json'
+json_str = '[{"name":"Apple","colors":["red","green","yellow"],"in_season":true},{"name":"Orange","colors":["orange"],"in_season":true},{"name":"Grapes","colors":["purple","green"],"in_season":false},{"name":"Lime","colors":["green"],"in_season":false},{"name":"Banana","colors":["yellow"],"in_season":false},{"name":"Watermelon","colors":["red"],"in_season":false},{"name":"Blueberry","colors":["blue"],"in_season":true},{"name":"Coconut","colors":["white"],"in_season":true}]'
+
+data = JSON.parse(json_str)
+Fruit.create(data)
