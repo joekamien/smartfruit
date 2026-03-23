10349e032b5b1ce22d5ad603b325991f818a21e1
Joe Kamien <14943848+joekamien@users.noreply.github.com>
2026-03-22 17:14:29 -0700

    db updates for migration

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,13 +3,4 @@ set -o errexit
 
 bundle install
 bundle exec rails assets:precompile
-
-# For now, the app should always have the same data
-# so reset it on every deployment.
-# But if this ever becomes a real app with real user data,
-# we can turn off this behavior with an environment variable.
-if [ "$RESET_DB_ON_DEPLOY" = "true" ]; then
-  bundle exec rails db:reset
-else
-  bundle exec rails db:prepare
-fi
+bundle exec rails db:migrate
